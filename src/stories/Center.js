e553d63bb527fd1b029d694bc5e86b0c9c418f4d
Wendreo Matheus <wendreo.pvh@gmail.com>
2020-02-19 09:53:18 -0500

    Configurado StoryBook

---
+++ src/stories/Center.js
@@ -0,0 +1,4 @@
+import React from 'react';
+const styles = {width: '100%', height: '100%',  display: 'flex', justifyContent: 'center', alignItems: 'center'}
+
+export default ({children}) => <div style={styles}> {children} </div>
