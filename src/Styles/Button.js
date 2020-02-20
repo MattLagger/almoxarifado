e55f20ee15362428ea994b3008ffa8ec2debcd8e
Wendreo Matheus <wendreo.pvh@gmail.com>
2020-02-20 15:05:47 -0500

    Ajuste nas Cores

--- src/Styles/Button.js
+++ src/Styles/Button.js
@@ -1,5 +1,38 @@
-import styled from "styled-components";
+import styled from "styled-components"
+import Btn from '@material-ui/core/Button'
+import {green, red, blue} from 'ui-colors'
 
-export default styled.button.attrs({
-    className: 'button'
-})``
+const setColor = (color) => {
+    let variant = green;
+
+    if (color === 'secondary')
+        variant = red;
+    else if (color === 'alternative')
+        variant = blue;
+    
+    console.log(color)
+    console.log(variant)
+
+    return variant;
+}
+
+
+export const Button = styled(Btn).attrs({
+    variant: 'contained',
+    disableElevation: true
+})`
+    &&{
+        color: ${props => props.outlined ? setColor(props.color) : 'white'};
+        background-color: ${props => setColor(props.color)};
+    }
+`
+
+export const ButtonOutlined =  styled(Btn).attrs({
+    variant: 'outlined',
+    disableElevation: true
+})`
+    &&{
+        color: ${props => setColor(props.color)};
+        border-color: ${props => setColor(props.color)};
+    }
+`
---
+++ src/Styles/Colors.js
@@ -0,0 +1,11 @@
+export default {
+    green: '#51B496',
+    red: '#EF8884',
+    blue: '#2E74B5',
+    text1: '#4B4C4D',
+    text2: '#7F8081',
+    text3: '#808182',
+    background1: '#FFFFFF',
+    background2: #F6F7F8,
+    background3: #F6F7F8,
+}
